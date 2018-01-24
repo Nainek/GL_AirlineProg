19bcf0b816fcc7a49b5bdb44b97ffb4afcbc6ac4
Nainek <Nainek95@gmail.com>
2018-01-24 14:37:01 +0200

    CargoAirliner created with cargoHoldCapasity field

---
+++ src/main/java/ua/com/glprocamp/airline/model/entities/CargoAirliner.java
@@ -0,0 +1,16 @@
+package ua.com.glprocamp.airline.model.entities;
+
+public class CargoAirliner extends Airliner {
+
+    private int cargoHoldCapasity;
+
+
+
+    public int getCargoHoldCapasity() {
+        return cargoHoldCapasity;
+    }
+
+    public void setCargoHoldCapasity(int cargoHoldCapasity) {
+        this.cargoHoldCapasity = cargoHoldCapasity;
+    }
+}
