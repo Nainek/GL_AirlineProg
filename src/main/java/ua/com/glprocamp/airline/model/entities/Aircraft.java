cc3723047489507c5ed5a8869c681f4dc7e3d4c9
Nainek <Nainek95@gmail.com>
2018-01-24 11:57:26 +0200

    Getters and Setters was added

--- src/main/java/ua/com/glprocamp/airline/model/entities/Aircraft.java
+++ src/main/java/ua/com/glprocamp/airline/model/entities/Aircraft.java
@@ -5,4 +5,19 @@ public abstract class Aircraft {
     private MethodOfLift methodOfLift;
     private int flightRange;
 
+    public MethodOfLift getMethodOfLift() {
+        return methodOfLift;
+    }
+
+    public void setMethodOfLift(MethodOfLift methodOfLift) {
+        this.methodOfLift = methodOfLift;
+    }
+
+    public int getFlightRange() {
+        return flightRange;
+    }
+
+    public void setFlightRange(int flightRange) {
+        this.flightRange = flightRange;
+    }
 }
