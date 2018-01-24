0434f078261b160148e7cf1511dfa7d1f6a839b7
Nainek <Nainek95@gmail.com>
2018-01-24 15:04:22 +0200

    Added new implementation of Calculation strategy

---
+++ src/main/java/ua/com/glprocamp/airline/model/calculations/CapasityCalculation.java
@@ -0,0 +1,21 @@
+package ua.com.glprocamp.airline.model.calculations;
+
+import ua.com.glprocamp.airline.model.entities.AirLine;
+import ua.com.glprocamp.airline.model.entities.Airliner;
+
+import java.util.Iterator;
+
+
+public class CapasityCalculation implements CalculationStrategy<AirLine, Integer> {
+
+    public Integer calculate(AirLine airLine) {
+        int result = 0;
+
+        Iterator<Airliner> iterator = airLine.getAirliners().iterator();
+        while(iterator.hasNext()){
+            result += iterator.next().getCarryingCapasity();
+        }
+        return result;
+    }
+
+}
--- src/main/java/ua/com/glprocamp/airline/model/entities/AirLine.java
+++ src/main/java/ua/com/glprocamp/airline/model/entities/AirLine.java
@@ -1,10 +1,13 @@
 package ua.com.glprocamp.airline.model.entities;
 
 import java.util.List;
+import java.util.Spliterators;
 
 public class AirLine {
 
-    List<PassengerAirliner> pLiners;
+
+    private List<Airliner> airliners;
+    private List<PassengerAirliner> pLiners;
 
 
     public List<PassengerAirliner> getPLiners() {
@@ -14,4 +17,22 @@ public class AirLine {
     public void setPAirliners(List<PassengerAirliner> pLiners) {
         this.pLiners = pLiners;
     }
+
+    public List<Airliner> getAirliners() {
+        return airliners;
+    }
+
+    public void setAirliners(List<Airliner> airliners) {
+        this.airliners = airliners;
+    }
+
+    public void addAirliner(Airliner airliner){
+        airliners.add(airliner);
+        if(airliner instanceof PassengerAirliner){
+            pLiners.add((PassengerAirliner) airliner);
+        }
+
+    }
+
+
 }
