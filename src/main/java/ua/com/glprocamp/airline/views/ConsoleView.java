23e325064f30398ad75862b0b9b6b707c960343e
Nainek <Nainek95@gmail.com>
2018-01-24 11:16:46 +0200

    Created class ConsoleView and method displayMessage was implemented

---
+++ src/main/java/ua/com/glprocamp/airline/views/ConsoleView.java
@@ -0,0 +1,9 @@
+package ua.com.glprocamp.airline.views;
+
+public class ConsoleView implements View{
+
+
+    public void displayMessage(String message) {
+        System.out.println(message);
+    }
+}
