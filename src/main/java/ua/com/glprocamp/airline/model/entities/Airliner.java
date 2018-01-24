69855c8a85db977907f369a615b6b5a23b8ea8d6
Nainek <Nainek95@gmail.com>
2018-01-24 11:57:40 +0200

    Getters and Setters was added

--- src/main/java/ua/com/glprocamp/airline/model/entities/Airliner.java
+++ src/main/java/ua/com/glprocamp/airline/model/entities/Airliner.java
@@ -5,4 +5,19 @@ public abstract class Airliner extends Aircraft {
     private Crew crew;
     private int fuelConsumption;
 
+    public Crew getCrew() {
+        return crew;
+    }
+
+    public void setCrew(Crew crew) {
+        this.crew = crew;
+    }
+
+    public int getFuelConsumption() {
+        return fuelConsumption;
+    }
+
+    public void setFuelConsumption(int fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+    }
 }
