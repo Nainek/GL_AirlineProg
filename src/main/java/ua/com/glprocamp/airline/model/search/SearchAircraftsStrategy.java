193c5bf64c42fef7657a96676bda7511a1e5d875
Nainek <Nainek95@gmail.com>
2018-01-24 12:21:51 +0200

    Added public mehtod

--- src/main/java/ua/com/glprocamp/airline/model/search/SearchAircraftsStrategy.java
+++ src/main/java/ua/com/glprocamp/airline/model/search/SearchAircraftsStrategy.java
@@ -1,4 +1,9 @@
 package ua.com.glprocamp.airline.model.search;
 
-public interface SearchAircraftsStrategy {
+import java.util.List;
+
+public interface SearchAircraftsStrategy<T>{
+
+    List<T> findAircraftInTheSpecifiedRange(List<T> aircrafts, int start, int end);
+
 }
