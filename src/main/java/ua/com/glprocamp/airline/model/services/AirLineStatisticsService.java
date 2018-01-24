73c2bac69d38b6ea02aeed8cfee1a74402f534a9
Nainek <Nainek95@gmail.com>
2018-01-24 15:08:09 +0200

    Realized two methods for statistic calculation

--- src/main/java/ua/com/glprocamp/airline/model/services/AirLineStatisticsService.java
+++ src/main/java/ua/com/glprocamp/airline/model/services/AirLineStatisticsService.java
@@ -1,6 +1,7 @@
 package ua.com.glprocamp.airline.model.services;
 
-import ua.com.glprocamp.airline.model.calculations.CalculationStrategy;
+
+import ua.com.glprocamp.airline.model.calculations.CapasityCalculation;
 import ua.com.glprocamp.airline.model.calculations.PassengerCapasityCalculation;
 import ua.com.glprocamp.airline.model.entities.AirLine;
 
@@ -10,9 +11,12 @@ public class AirLineStatisticsService {
     public int calculateSeatingCapasity(AirLine airLine){
 
         return new PassengerCapasityCalculation().calculate(airLine);
-        }
 
-    public int calculateCarryingCapasity(){
+    }
+
+    public int calculateCarryingCapasity(AirLine airLine){
+
+        return new CapasityCalculation().calculate(airLine);
 
     }
 
