862469fa2ce0789261ebbc7416e83d8333a1d444
Nainek <Nainek95@gmail.com>
2018-01-24 14:35:32 +0200

    Method calculateSeatingCapasity realized

--- src/main/java/ua/com/glprocamp/airline/model/services/AirLineStatisticsService.java
+++ src/main/java/ua/com/glprocamp/airline/model/services/AirLineStatisticsService.java
@@ -1,13 +1,15 @@
 package ua.com.glprocamp.airline.model.services;
 
+import ua.com.glprocamp.airline.model.calculations.CalculationStrategy;
+import ua.com.glprocamp.airline.model.calculations.PassengerCapasityCalculation;
 import ua.com.glprocamp.airline.model.entities.AirLine;
 
 public class AirLineStatisticsService {
 
-    public int calculateSeatingCapasity(AirLine airLine){
-        int result = 0;
 
-        return result;
+    public int calculateSeatingCapasity(AirLine airLine){
+
+        return new PassengerCapasityCalculation().calculate(airLine);
         }
 
     public int calculateCarryingCapasity(){
