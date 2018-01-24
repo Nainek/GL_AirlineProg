fdda2f81ccbd11e87c82e98e1682c7cf5b8608ad
Nainek <Nainek95@gmail.com>
2018-01-24 12:39:19 +0200

    Comparator By Flight Range and interface for that was crated and filled up with appropriate methods

---
+++ src/main/java/ua/com/glprocamp/airline/model/comparators/AircraftComparator.java
@@ -0,0 +1,12 @@
+package ua.com.glprocamp.airline.model.comparators;
+
+import ua.com.glprocamp.airline.model.entities.Aircraft;
+
+import java.util.Comparator;
+
+public interface AircraftComparator  extends Comparator<Aircraft> {
+
+        int compare(Aircraft o1, Aircraft o2);
+
+}
+
