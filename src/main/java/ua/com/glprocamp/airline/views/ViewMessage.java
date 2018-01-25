a46ae9b60581623f1e6a00ae55e89386ffbbe4fe
Nainek <Nainek95@gmail.com>
2018-01-25 13:11:44 +0200

    Changes in access to the properties

---
+++ src/main/java/ua/com/glprocamp/airline/views/ViewMessage.java
@@ -0,0 +1,9 @@
+package ua.com.glprocamp.airline.views;
+
+public interface ViewMessage {
+
+    String HELLO_MESSAGE = "hello.message";
+    String OFFER_MESSAGE = "offer.message";
+    String WRONG_INPUT_INT_DATA = "input.wrong.data";
+
+}
---
+++ src/main/java/ua/com/glprocamp/airline/views/utils/ViewMessageUtils.java
@@ -0,0 +1,17 @@
+package ua.com.glprocamp.airline.views.utils;
+
+public interface ViewMessageUtils {
+
+    String LEFT_PARENTHESIS = "{ ";
+    String RIGHT_PARANTHESIS = " }";
+    String COMMA = ", ";
+    String FULL_STOP = ".";
+    String COLON = ": ";
+    String EMPTY_STR = " ";
+    String NEW_LINE = "\n";
+    String BACKSLASH = "/";
+    String SEMICOLON = ";";
+    String VERT_LINA = " |";
+    String EQUALITY_SIGN = " = ";
+
+}
